9de6ffb2d203990cb08555ed3eb8c50f1a7d12e5
wil974-dev <wggron99@gmail.com>
2025-05-23 11:01:20 +0400

    Modifications mineures

--- src/components/Presentation/index.jsx
+++ src/components/Presentation/index.jsx
@@ -6,16 +6,16 @@ function Presentation() {
             <p className="presentation__texte">
                 <span className="presentation__nom">GRONDIN William,</span>
                 <br />
-                j'ai 41 ans J’ai suivi une formation à distance de développeur
-                web avec OpenClassrooms, qui m’a transmis les clés essentielles
-                pour concevoir des sites web modernes. Passionné par la création
+                j'ai 41 ans, j’ai suivi une formation à distance de développeur
+                web avec OpenClassrooms, qui m’a transmis les compétences pour
+                concevoir des sites web modernes. Passionné par la création
                 d’expériences numériques accessibles, intuitives et utiles, je
                 suis une personne curieuse, autonome et rigoureuse. Je me forme
                 continuellement pour maîtriser les technologies du web, aussi
                 bien côté front-end que back-end. Je vous invite à consulter mon
                 portfolio, qui présente mes projets les plus significatifs :
                 chacun d’eux reflète mon évolution, mon implication et les
-                compétences que j’ai acquises. Je suis à la recherche de
+                compétences que j’ai développées. Je suis à la recherche de
                 nouvelles opportunités pour mettre mes compétences au service de
                 projets innovants. N’hésitez pas à me contacter !
             </p>
